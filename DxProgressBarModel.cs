45d58508036af33875679f1f67765170f72e6e08
FarzadJM <farzadjalalimajd@gmail.com>
2025-07-16 11:36:59 +0330

    Add project files.

---
+++ DxProgressBarModel.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.Blazor;
+using DevExpress.ExpressApp.Blazor.Components.Models;
+using System.ComponentModel;
+
+namespace JMTech.ExpressApp.Blazor.Editors.Models;
+
+public class DxProgressBarModel : ComponentModelBase
+{
+    public bool Indeterminate
+    {
+        get => GetPropertyValue<bool>(false);
+        set => SetPropertyValue(value);
+    }
+
+    public string Label
+    {
+        get => GetPropertyValue<string>();
+        set => SetPropertyValue(value);
+    }
+
+    public ProgressBarLabelPosition LabelPosition
+    {
+        get => GetPropertyValue<ProgressBarLabelPosition>(ProgressBarLabelPosition.Bottom);
+        set => SetPropertyValue(value);
+    }
+
+    public double MinValue
+    {
+        get => GetPropertyValue<double>(0);
+        set => SetPropertyValue(value);
+    }
+
+    public double MaxValue
+    {
+        get => GetPropertyValue<double>(100);
+        set => SetPropertyValue(value);
+    }
+
+    public bool ShowIcon
+    {
+        get => GetPropertyValue<bool>(false);
+        set => SetPropertyValue(value);
+    }
+
+    public bool ShowLabel
+    {
+        get => GetPropertyValue<bool>(true);
+        set => SetPropertyValue(value);
+    }
+
+    public string CssClass
+    {
+        get => GetPropertyValue<string>();
+        set => SetPropertyValue(value);
+    }
+
+    public SizeMode? SizeMode
+    {
+        get => GetPropertyValue<SizeMode?>();
+        set => SetPropertyValue(value);
+    }
+
+    public ProgressBarStatus Status
+    {
+        get => GetPropertyValue<ProgressBarStatus>(ProgressBarStatus.InProgress);
+        set => SetPropertyValue(value);
+    }
+
+    public string Thickness
+    {
+        get => GetPropertyValue<string>();
+        set => SetPropertyValue(value);
+    }
+
+    public ProgressBarType Type
+    {
+        get => GetPropertyValue<ProgressBarType>(ProgressBarType.Horizontal);
+        set => SetPropertyValue(value);
+    }
+
+    public double Value
+    {
+        get => GetPropertyValue<double>(0);
+        set => SetPropertyValue(value);
+    }
+
+    public bool Visible
+    {
+        get => GetPropertyValue<bool>(true);
+        set => SetPropertyValue(value);
+    }
+
+    public override Type ComponentType => typeof(DevExpress.Blazor.DxProgressBar);
+}
---
+++ DxProgressBarPropertyEditor.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using DevExpress.ExpressApp.Blazor.Editors;
+using DevExpress.ExpressApp.Model;
+using JMTech.ExpressApp.Blazor.Editors.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace JMTech.ExpressApp.Blazor.Editors;
+
+public class DxProgressBarPropertyEditor : BlazorPropertyEditorBase
+{
+    public DxProgressBarPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+
+    public override DxProgressBarModel ComponentModel => (DxProgressBarModel)base.ComponentModel;
+
+    protected override IComponentModel CreateComponentModel() => new DxProgressBarModel() { CssClass = "w-100" };
+
+    protected override RenderFragment CreateViewComponentCore(object dataContext)
+    {
+        var componentModel = new DxProgressBarModel
+        {
+            Value = (double)(this.GetPropertyValue(dataContext) ?? 0),
+            Thickness = "4px",
+            ShowLabel = false,
+            CssClass = "w-100",
+            SizeMode = DevExpress.Blazor.SizeMode.Small
+        };
+
+        return componentModel.GetComponentContent();
+    }
+
+    protected override void ReadValueCore()
+    {
+        base.ReadValueCore();
+        ComponentModel.Value = (double)PropertyValue;
+    }
+}
